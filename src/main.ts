6daa7fe375fe731fbf6bd3360681a9630c984abf
Victor Oliveira <victor.oliveira@avanade.com>
2024-06-07 17:14:44 -0300

    fix: melhorias pontuais

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+declare global {
+    interface Window {
+        callbackSucesso: any;
+        callbackErro: any;
+    }
+}
+
 window.callbackSucesso = (mensagem: string, chave: string) => {
     var promise = promisesCallback.find(item => {
         return item.chave == chave;
     });
-
     promise?.promiseResolve({ mensagem, chave });
 }
 
@@ -10,13 +16,12 @@ window.callbackErro = (mensagem: string, chave: string) => {
     var promise = promisesCallback.find(item => {
         return item.chave == chave;
     });
-
     promise?.promiseReject({ mensagem, chave });
 }
 
 var promisesCallback: [{ chave: string, promiseResolve: any, promiseReject: any }];
 
-function comunicacaoNativo(parametro: any) {
+export function comunicacaoNativo(parametro: any) {
     let chavePromise = Math.floor(Math.random() * 99999).toString();
     parametro.chave = chavePromise;
     window['Native']['execute'](JSON.stringify(parametro));
@@ -28,6 +33,4 @@ function comunicacaoNativo(parametro: any) {
             promisesCallback = [{ chave: chavePromise, promiseResolve: resolve, promiseReject: reject }];
         }
     });
-}
-
-export comunicacaoNativo;
+};
