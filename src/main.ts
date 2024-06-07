68f546f43efcd29d9595f4e01bd8b8f697f3eba8
Victor Oliveira <victor.oliveira@avanade.com>
2024-06-07 16:13:52 -0300

    fix: adicionando o brigde

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+window.callbackSucesso = (mensagem: string, chave: string) => {
+    var promise = promisesCallback.find(item => {
+        return item.chave == chave;
+    });
+
+    promise?.promiseResolve({ mensagem, chave });
+}
+
+window.callbackErro = (mensagem: string, chave: string) => {
+    var promise = promisesCallback.find(item => {
+        return item.chave == chave;
+    });
+
+    promise?.promiseReject({ mensagem, chave });
+}
+
+var promisesCallback: [{ chave: string, promiseResolve: any, promiseReject: any }];
+
+function comunicacaoNativo(parametro: any) {
+    let chavePromise = Math.floor(Math.random() * 99999).toString();
+    parametro.chave = chavePromise;
+    window['Native']['execute'](JSON.stringify(parametro));
+
+    return new Promise((resolve, reject) => {
+        if (promisesCallback) {
+            promisesCallback.push({ chave: chavePromise, promiseResolve: resolve, promiseReject: reject })
+        } else {
+            promisesCallback = [{ chave: chavePromise, promiseResolve: resolve, promiseReject: reject }];
+        }
+    });
+}
+
+export comunicacaoNativo;
